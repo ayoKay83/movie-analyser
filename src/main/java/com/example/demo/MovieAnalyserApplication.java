431e9b4eb85c7359831b557cac895f77d90b22a0
dean <nifr@kea.dk>
2021-02-10 18:01:55 +0100

    First

--- src/main/java/com/example/demo/MovieAnalyserApplication.java
+++ src/main/java/com/example/demo/MovieAnalyserApplication.java
@@ -3,11 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class MovieAnalyserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieAnalyserApplication.class, args);
 	}
-
 }
